4a78f850b9e8941cd8ce31d9062ed41f0851a46a
Fethican Gulcebi <firblo@hotmail.com>
2024-02-12 13:44:19 +0100

    3 reviews geplaatst invulpagina. Verbeteringen gezet in bovenheader

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php  
+    $fullName = $_POST['fullName'];
+    $date = $_POST['date'];
+    $persoonlijk_bericht = $_POST['persoonlijk_bericht'];
+    $image = $_POST['image'];
+    
+    //Database connection
+?>
+
+
